d987b94277db41842c9c5f578edea3caa33fc2f7
cestjoanna <33450692+cestjoanna@users.noreply.github.com>
2018-01-12 09:43:44 +0100

    Create profil_client_view.php

---
+++ profil_client_view.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="../style/profil_client.css"  >
+        <title>Profil du client</title>
+    </head>
+
+
+    
+    <body>
+    
+    <?php include("../view/header.html");?>
+
+    <div class="carre">
+    <h1>Votre profil</h1>
+    <?php
+    require ("../connect.php");
+   
+    $req = $bdd->query('SELECT ID_user,name,email,phone_number,account_creation_date,last_connection_date,is_connected,is_admin,admin_authorization,last_cgu_acceptance FROM users ORDER BY account_creation_date DESC LIMIT 0, 1');
+       while ($donnees = $req->fetch()){
+          
+       ?>
+
+        <p><span> Votre ID : </span><?php echo ($donnees['ID_user']); ?></p>
+        <p><span> Votre nom : </span><?php echo ($donnees['name'] );?></p>
+        <p><span> Votre Email : </span><?php echo ($donnees['email']); ?></p>
+        <p><span> Téléphone : </span><?php echo ($donnees['phone_number']); ?></p>
+    
+        
+       
+<?php }
+?>
+
+<form method="POST" action="controler/modif_profil.php" >
+            <input type="submit" value="Modifier le profil" name="submit"/>
+        </form>
+
+</div>
+</body>
+<?php include("../view/footer.php");?>
+</html>
