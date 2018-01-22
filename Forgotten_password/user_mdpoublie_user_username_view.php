4a152b1042b26b4c5c26878be69f9fae77042f10
Teva Paquin <34157569+posesousjack@users.noreply.github.com>
2018-01-22 12:37:37 +0100

    Add files via upload

---
+++ Forgotten_password/user_mdpoublie_user_username_view.php
@@ -0,0 +1,19 @@
+<?php
+/* Author : Teva Paquin
+version : 1.0
+date : 12/12
+*/
+?>
+
+  
+    <h1>Mot de passe oublié</h1>
+    <div>
+        <p> Quel est votre nom d'utilisateur? <br/></p>
+
+        <form method='POST' action=''>
+            <label>Réponse : <input type='text' name='reponse' class='reponse' placeholder='Ex:Gontrand'autofocus required maxlength=256/> </label><br>
+            <label><input type='submit' name='submit' class='ajout' value='Ajout'/></label>
+    
+        </form>
+       
+    </div>
