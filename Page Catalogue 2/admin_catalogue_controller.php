b8c87078fa2e8a88e544caf84340f3708aad4432
Teva Paquin <34157569+posesousjack@users.noreply.github.com>
2018-01-22 13:08:00 +0100

    Add files via upload

---
+++ Page Catalogue 2/admin_catalogue_controller.php
@@ -0,0 +1,45 @@
+<?php
+/* Author : Teva Paquin
+version : 1.0
+date : 13/12
+*/
+
+require ($localisation.'admin/model/admin_catalogue_model.php');
+empty ($_POST['submit']);
+
+if (isset($_POST['submit']))
+    {   
+    add_product($bdd,(input_securisation($_POST['product_name'])),(input_securisation($_POST['price'])),(input_securisation($_POST['conso'])),(input_securisation($_POST['description'])),(input_securisation($_POST['type_device'])));
+    require ($localisation.'admin/view/admin_catalogue_view.php'); 
+    }
+else
+{
+    if (isset($_POST['supprimer']))
+    {
+    del_product($bdd,(input_securisation($_POST['ID'])));
+    require ($localisation.'admin/view/admin_catalogue_view.php'); 
+    }
+    else
+    {
+        if(isset($_POST['modifier']))
+        {
+            require ($localisation.'admin/view/admin_catalogue_modif_view.php');
+        }
+        else
+        {
+            if (isset($_POST['modifier2']))
+            {
+                modif_product($bdd,(input_securisation($_POST['ID'])),(input_securisation($_POST['product_namemodif'])),(input_securisation($_POST['pricemodif'])),(input_securisation($_POST['consomodif'])),(input_securisation($_POST['descriptionmodif'])),(input_securisation($_POST['type_device_modif'])));
+                require ($localisation.'admin/view/admin_catalogue_view.php'); 
+            }
+            else
+            {
+                require ($localisation.'admin/view/admin_catalogue_view.php');
+            }
+        }
+    }
+}
+
+
+
+?>
