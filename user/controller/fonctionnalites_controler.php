eb5b9ff840666b9dad859ba91c64e13a78cc4ee2
cestjoanna <33450692+cestjoanna@users.noreply.github.com>
2018-01-22 11:20:15 +0100

    Add files via upload

---
+++ user/controller/fonctionnalites_controler.php
@@ -0,0 +1,80 @@
+<?php
+  
+  $_SESSION['ID_installation']=2;
+  require ($localisation.'user/view/fonctionnalites.php');
+
+if (isset($_POST['lumiere_all']) AND isset($_POST['submit']) AND isset($_POST['selec_salle'])) 
+  { 
+    require ($localisation.'user/model/fonctionnalites_model.php');
+    lumiere_all($bdd);
+
+
+  }
+
+
+if (isset($_POST['lumiere_et']) AND isset($_POST['submit']) AND isset($_POST['selec_salle'])) 
+  { 
+    require ($localisation.'user/model/fonctionnalites_model.php');
+    lumiere_et($bdd);
+
+  }
+
+if (isset($_POST['chauffage_all']) AND isset($_POST['submit']) AND isset($_POST['selec_salle'])) 
+  { 
+    require ($localisation.'user/model/fonctionnalites_model.php');
+    chauffage_all($bdd);
+
+  }
+
+if (isset($_POST['chauffage_et']) AND isset($_POST['submit']) AND isset($_POST['selec_salle'])) 
+  { 
+    require ($localisation.'user/model/fonctionnalites_model.php');
+    chauffage_et($bdd);
+
+  }
+
+if (isset($_POST['fen_ouvre']) AND isset($_POST['submit']) AND isset($_POST['selec_salle'])) 
+  { 
+    require ($localisation.'user/model/fonctionnalites_model.php');
+    fen_ouvre($bdd);
+  }
+
+if (isset($_POST['fen_ferme']) AND isset($_POST['submit']) AND isset($_POST['selec_salle'])) 
+  { 
+    require ($localisation.'user/model/fonctionnalites_model.php');
+    fen_ferme($bdd);
+
+  }
+
+if (isset($_POST['ouvr_volets']) AND isset($_POST['submit']) AND isset($_POST['selec_salle'])) 
+  { 
+    require ($localisation.'user/model/fonctionnalites_model.php');
+    ouvr_volets($bdd);
+
+  }
+
+if (isset($_POST['ferme_volets']) AND isset($_POST['submit']) AND isset($_POST['selec_salle'])) 
+  { 
+    require ($localisation.'user/model/fonctionnalites_model.php');
+    ferme_volets($bdd);
+
+  }
+
+if ( isset($_POST['submit2']) AND isset($_POST['id_capteur']) AND $_POST['act_inactif']=='active') 
+  { 
+    require ($localisation.'user/model/fonctionnalites_model.php');
+
+    activer_capteur($bdd);
+
+  }
+
+if ( isset($_POST['submit2']) AND isset($_POST['id_capteur']) AND $_POST['act_inactif']=='inactive') 
+  { 
+    require ($localisation.'user/model/fonctionnalites_model.php');
+
+    desactiver_capteur($bdd);
+    
+
+  }
+
+?>
