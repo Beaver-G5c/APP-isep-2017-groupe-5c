add382e65cc9baeaed749cacda3bc19f8910e2c6
cestgeoffrey <33450309+cestgeoffrey@users.noreply.github.com>
2018-01-24 19:00:06 +0100

    affichage du catalogue

--- getinfomodel.php
+++ getinfomodel.php
@@ -1,37 +1,30 @@
 <?php
-
-$bdd = (new PDO('mysql:host=localhost;dbname=app;charset=utf8', 'root', 'root'));
-$req = $bdd->query('SELECT * FROM catalogue');
-$donnees = $req->fetch();
-
-
-
-if(isset($_POST['valider_ajout_salle'])){
-    require_once 'getinfoview.php';
-    echo"ca marche";
-    $bdd = (new PDO('mysql:host=localhost;dbname=app;charset=utf8', 'root', 'root'));
-    $req = $bdd->query('SELECT * FROM catalogue');
-    $post_name_id=$_POST['name_id'];
+function getCatalogue($bdd){
     
-    foreach ($post_name_id as $key=>$value){
-        echo $value;
-        echo $key;
+    $req = $bdd->query('SELECT * FROM catalogue');
+    $dataArray=array('ID_type'=>array(),'product_name'=>array());
+    $i=0;
+    while($donnees=$req->fetch()){
+        $dataArray['ID'][$i]=$donnees['ID'];
+        $dataArray['ID_type'][$i]=$donnees['ID_type'];
+        $dataArray['product_name'][$i]=$donnees['product_name'];
         
-        
-        
-        $requeteajouter = $bdd->prepare("INSERT INTO capteurs(id_sensor) VALUES('.$value.') ");
-        $requeteajouter->execute();
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    }}
+        $i++;
+    }
+    
+    return $dataArray;
+    
+}
+
+
+function add_device ($bdd,$id,$room_name,$id_room,$id_sensor,$id_type,$id_install){
+    echo ($id);
+    $requeteajouter = $bdd->prepare("INSERT INTO capteurs(nomdelasalle,id_salle,id_sensor,type,id_installation) VALUES(:nomdelasalle, :id_salle, :id_sensor, :type, :id_installation) ");
+    $requeteajouter->execute(array('nomdelasalle'=>$room_name,'id_salle'=>$id_room, 'id_sensor'=>$id, 'type'=>$id_type, 'id_installation'=>$id_install));
+    
+}
+?>
+    
     
 
 
--- getinfoview.php
+++ getinfoview.php
@@ -2,39 +2,26 @@
 
 
 
-function catalogue(){
-    //Permet d'afficher les capteurs et actionneurs présents dans le catalogue Domisep
-  require 'getinfomodel.php';
-    $i=0;
-    foreach ($req as $value){
-        
+function displayCatalogue($data){
+    for ($i=0;$i<count($data['ID']);$i++){ ?>
+            
+        <input type="checkbox" name="<?php echo('checkbox'.$data['ID'][$i]); ?>" /><label ><?php echo($data['product_name'][$i]); ?></label><input type="text" name="<?php echo('cat'.$data['ID'][$i]);?>"  />
+   		<input type='hidden' name='<?php echo('id_type'.$data['ID'][$i]);?> value='<?php echo($data['ID_type'][$i])?>'/>
+    <?php  }
+    
 
-    ?>
-	<form method="post">
-  <div>
-    <input type="checkbox" >
-    <label for="multifonction"><?php echo($value['product_name']); ?></label>
-    <input type="text" name="name_id" placeholder="id <?php echo($value['product_name']); ?>">
-     
-  </div></form>
-  <?php
-  $i++;
-    }}
-
-echo catalogue();
-?><form method="POST" action="">
-
-<input type="submit" name="valider_ajout_salle">
-
-</form>
-<?php 
-
-
-
-
-
+}
 
 ?>
+<?php function sensorList($data){?>
+    <form method="POST" action="">
+		<?php displayCatalogue($data);?>
+		<input type='submit' name='ajout_capteur' value='ajouter'/>
+	</form>
+	
+<?php }?>
+
+
 
 
 
