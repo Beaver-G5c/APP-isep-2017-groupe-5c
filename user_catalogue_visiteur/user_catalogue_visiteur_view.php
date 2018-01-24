abfa5c65fde18b46a4d85bf99b8e35aa06f9cac2
Teva Paquin <34157569+posesousjack@users.noreply.github.com>
2018-01-24 16:09:07 +0100

    Add files via upload

---
+++ user_catalogue_visiteur/user_catalogue_visiteur_view.php
@@ -0,0 +1,27 @@
+
+
+<?php
+/* Author : Teva Paquin
+version : 1.0
+date : 12/12
+*/
+
+
+?>
+
+ 
+    <div id='show_product'>
+        <h1>Nos produits en vente</h1>
+    <table>
+    <tr>
+    <td><b>ID</b></td>
+    <td><b>Nom du produit</b></td>
+    <td><b>Prix</b></td>
+    <td><b>Consommation</b></td>
+    <td><b>Description</b></td>
+    <td><b>Type</b></td>
+    <?php show_product($bdd) ?>
+    
+
+</table>
+</div>
