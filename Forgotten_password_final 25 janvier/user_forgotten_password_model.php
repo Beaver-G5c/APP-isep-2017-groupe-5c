3c7f53f3776f2e323c18e313def3ba27572cd064
Teva Paquin <34157569+posesousjack@users.noreply.github.com>
2018-01-25 11:48:33 +0100

    Add files via upload

---
+++ Forgotten_password_final 25 janvier/user_forgotten_password_model.php
@@ -0,0 +1,60 @@
+<?php
+/* Auteur : Teva
+Version : 1.0
+Date : 08/01
+*/
+function verification($bdd,$user_name){
+$reponse = $bdd->query('SELECT * FROM users');
+$douille = 0;
+while ($donnees = $reponse->fetch())
+{
+if ($user_name == $donnees['name'])
+{
+    $douille = $douille + 1;
+}    
+}
+if ($douille == 1)
+{
+    return 1;
+}
+}
+function verification2($bdd,$secret_answer)
+{
+    
+    $reponse = $bdd->query('SELECT * FROM users');
+$douille = 0;
+while ($donnees = $reponse->fetch())
+{
+if ($secret_answer == $donnees['secret_answer'])
+{
+    $douille = $douille + 1;
+}    
+}
+if ($douille == 1)
+{
+    return 1;
+}
+}
+function newmdp($bdd,$mdp,$name)
+{
+    
+    $req = $bdd->prepare('UPDATE users SET password = ? WHERE name = ?');
+    $req->execute(array($mdp,$name));
+   
+   
+}
+
+function secret_question($bdd,$nom){
+    $reponse = $bdd->query("SELECT * FROM `users` WHERE name ='". $nom."'");
+    while ($donnees = $reponse->fetch())
+            {
+                 echo ($donnees['secret_question']); 
+            }
+}
+            
+            
+           
+            
+            
+            
+?>
---
+++ Forgotten_password_final 25 janvier/user_mdpoublie_user_changementmdp_view.php
@@ -0,0 +1,21 @@
+<?php
+/* Author : Teva Paquin
+version : 1.0
+date : 12/12
+*/
+?>
+
+  
+    <h1>Nouveau mot de passe</h1>
+    <div id='new_mdp'>
+        <p> Quel sera votre nouveau mot de passe? <br/></p>
+
+        <form method='POST' action=''>
+            <label>Mot de passe : <input type='password' name='motdepasse' class='reponse' placeholder='Ex:********'autofocus required maxlength=256/> </label><br>
+            <label>Vérification : <input type='password' name='motdepasse2' class='reponse' placeholder='Ex:********'autofocus required maxlength=256/> </label><br>
+            <label><input type="hidden" value="<?php echo($_POST['reponse']);?>"  name="reponse" id="text2" /></label>
+            <label><input type='submit' name='submit3' class='ajout' value='Ajout'/></label>
+    
+        </form>
+       
+    </div>
