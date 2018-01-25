d30653b48db3856155e2551848c25d5cf72e33ed
Teva Paquin <34157569+posesousjack@users.noreply.github.com>
2018-01-25 11:44:12 +0100

    Add files via upload

---
+++ Page Catalogue final/admin_catalogue_view.php
@@ -0,0 +1,38 @@
+
+
+<?php
+/* Author : Teva Paquin
+version : 1.0
+date : 12/12
+*/
+
+?>
+
+    <h1>Ajout de produit</h1>
+    <div id='ajout_produit'>
+        <form method='POST' action=''>
+            <label>Nom du produit : <input type='text' name='product_name' class='product_name' placeholder='Ex:CC560'autofocus required maxlength=256/> </label><br>
+            <label>Prix : <input type='tel' name='price' class='price' placeholder=' Ex:250'required/></label><br>
+            <label>Consommation : <input type='tel'name='conso' class='conso' placeholder='404040404040' required maxlength=256/></label><br>
+            <label>Description : <input type='text' name='description' class='description' placeholder='Ex:Un capteur de température'required/></label><br>
+            <label>Type : <select id='type_device' name='type_device'>
+            <?php get_device_category($bdd); ?>
+            </select></label><br>   
+            <label><input type='submit' name='submit' class='ajout' value='Ajout'/></label>
+    
+        </form>
+       
+    </div>
+    <div id='produit_vente'>
+        <h1>Nos produits en vente</h1>
+    <table id='table_produit'>
+    <tr>
+    <td><b>ID</b></td>
+    <td><b>Nom du produit</b></td>
+    <td><b>Prix</b></td>
+    <td><b>Consommation</b></td>
+    <td><b>Description</b></td>
+    <td><b>Type</b></td>
+    <?php show_product($bdd); ?>
+    </table>
+    </div>
